822accec84fc5a590633dda032781a13530af9d6
asins <wkli@alibaba-inc.com>
2017-05-13 18:17:01 +0800

    重写代码

--- README.md
+++ README.md
@@ -1,24 +1,84 @@
-Gulp Git/Svn Version Filename
-=====================
-
+# Gulp Git/Svn Version Filename
 
 此 gulp 插件用于在文件名中添加对应 Git/Svn 的版本号。
 
 注意：`gulpfile.js` 文件所在目录需要是 Git/Svn 有效的版本仓库目录，并确保命令行中能正常运行 Git/Svn 命令。
 
-如何使用
-----------
+## 如何使用
+
 
 安装模块
 
 ```bash
 npm i gulp-git-svn-version-filename
 ```
+## 创建实例
+
+```js
+var FileVer = require('gulp-git-svn-version-filename');
+var fileVer = new FileVer({
+	type: 'svn', // 仓库类型 支持svn、git
+	cwd: process.cwd(), // 项目根目录
+	user: '', // 仓库账号名
+	pwd: '', // 仓库账号密码
+	cache: true, // 是否缓存已查询版本信息
+	formater: '{name}_{version}{ext}', // 文件名加版本号的规则（不提供目录结构修改）
+});
+```
+
+- type {String} 仓库类型 支持svn、git，默认为svn
+- cwd {String} 项目根目录，默认为process.cwd()
+- user {String} 仓库账号名，只支持SVN
+- pwd {String} 仓库账号密码 只支持SVN
+- cache {Boolean/Object} 是否缓存已查询版本信息
+  - Boolean时：允许与否。注意：为true时使用的是全局缓存，所有引用共用一个缓存空间。
+  - Object时：值为缓存对象，必须为普通对象()。
+- formater {String/Function}
+  - 值String时：文件名加版本号的规则（不提供目录结构修改） 默认值：'{name}_{version}{ext}'；
+  - 值为Function(pathObj, this)时：pathObj为`pathObj. = Path.parse(path)`，并加入`version`信息，this指向fileVer。
+
+### FileVer.cloneGitBaseRemot(remoteUrl, branche, callback)  静态方法 同步仓库的.git文件夹到本地当前目录
+- remoteUrl {String} 远程仓库地址
+- branche {String} 待clone的分支名
+- callback(err) clone完成后的回调方法，如果出错返回的第一个参数为error信息，如果成功则返回null
+
+### fileVer.getCache([path]) 获取已查询过的文件版本列表，数据为所有引用共用
+- path: 待查文件路径
+- 返回结果：当传入path参数时只返回path的对应版本信息, 否则返回所有缓存信息Map 如：`{'/home/user/git/project/js/a.js': '34ae3'}`
+
+### fileVer.delCache 清除所有缓存信息
+
+### fileVer.get(fullPath, callback) 获取指定URL的版本信息
+
+- fullPath {String} 绝对路径地址
+- callback(version) {Function} 获取到信息后的回调方法
+  - version {String} 为fullPath对应的版本号，当仓库中无版本号或获取失败时值为0
+
+### fileVer.formaterPath(fullPath, version)
+
+### fileVer.getForTransform(callback) 获取gulp(Transform方式)传入file的版本号信息
+
+- callback(version) {Function} 获取到信息后的回调方法
+  - version {String} 为fullPath对应的版本号，当仓库中无版本号或获取失败时值为null
+
+### fileVer.setForTransform(callback) 按formater给的规则设置gulp(TransForm方式)传入file的版本号
+- callback(oldFullFilePath, versionFullFilePath) 给`file.path`加完版本后的回调方法
+
+
+配置中user、pwd参数目前只有SVN支持，Git仓库请走ssh方式
 
 gulpfile.js 中
 
 ```js
-var fileVer = require('gulp-git-svn-version-filename');
+var FileVer = require('gulp-git-svn-version-filename');
+var cacheObj = {};
+var fileVer = new FileVer({
+	type: 'svn', // 仓库类型 支持svn、git
+	cwd: process.cwd(), // 项目根目录
+	user: '', // 仓库账号名
+	cache: cacheObj, // 指定缓存对象
+	pwd: '', // 仓库账号密码
+});
 gulp.task('addSvnVersion', function(){
 	return gulp.src(['./js/**/*.js', './css/**/*.css'])
 		.pipe(fileVer.addVersion({type: 'svn', callback: function(oldPath, versionPath){
--- index.js
+++ index.js
@@ -9,57 +9,277 @@ var Path = require('path');
 var logger = require('fancy-log');
 var chalk = require('chalk');
 var debug = require('debug')('gulp-git-svn-version-filename');
-var util = require('./util');
+var exec = require('child_process').exec;
 
-function addVersion(options) {
+var versionMap = {};
+var dotGitPath = Path.relative(process.cwd(), '.git'); //'./.git'; // .git目录路径
+
+
+function Versions(options){
+	Object.assign(this, {
+		type: 'git', // 默认使用git方式
+		user: '', // SVN中使用
+		pwd: '', // SVN中使用
+		cwd: '', // 文件的相对目录
+		cache: true,  // 是否开启缓存，或指定缓存对象
+		formater: '{name}_{version}{ext}',
+	}, options);
+}
+
+Versions.prototype.get = function(path, cb){
+    if(type(cb) !== 'function') cb = noop;
+
+	var getVersion = this.type === 'svn' ? getSvnVersion : getGitVersion;
+    if(path.indexOf('/') == 0){ // 相对.git文件夹路径
+        path = relativePath(path, this.cwd);
+    }
+
+    getVersion(path, {
+        user: this.user,
+        pwd: this.pwd,
+        cache: this.cache,
+    }, cb);
+};
+
+Versions.prototype.formaterPath = function(path, version){
+    var pathObj = Path.parse(path);
+    pathObj.version = version;
+
+    var fileName = type(this.formater) === 'function' ?
+        this.formater(pathObj, this) :
+        String(this.formater).replace(/\{(\w+)\}/ig, function ($0, name) {
+            var val = pathObj[name];
+            return type(val) === 'undefined' ? $0 : val;
+        });
+
+	return Path.resolve(pathObj.dir, fileName);
+};
+
+/**
+ * 影响tranform，获取文件的版本信息
+ */
+Versions.prototype.getForTransform = function(cb){
 	var stream = new Stream.Transform({
 		objectMode: true,
 	});
 
-	options = Object.assign({}, {
-		type: 'git', // 默认使用git方式
-		url: '', // 远程仓库地址
-		user: '', // SVN中使用
-		pwd: '', // SVN中使用
-		cwd: '', // 文件的相对目录
-		formater: '{name}_{version}{ext}',
-		callback: function(){}, // 操作完成后执行
-	}, options);
-
 	stream._transform = (file, filetype, callback) => {
-		var pathObj = Path.parse(file.path);
+        this.get(file.path, function(version){
+            if(type(cb) === 'function'){
+                cb(version, file);
+            }
 
-		(options.type == 'svn' ? util.getSvnVersioon : util.getGitVersion)(util.getRelativePath(file.path, options.cwd), options, (version) => {
-			if(!version)  return callback(null, file);
-
-			pathObj.version = version;
-
-			var fileName;
-			if(util.type(version) === 'function'){
-				fileName = options.formater(pathObj, options);
-			}else{
-				fileName = options.formater.replace(/\{(\w+)\}/ig, function($0, name){
-					var val = pathObj[name];
-					return util.type(val) === 'undefined' ? $0 : val;
-				});
-			}
-
-			var versionFilePath = Path.resolve(pathObj.dir, fileName);
-			logger(chalk.yellow(util.getRelativePath(file.path, options.cwd)), '->', chalk.green(util.getRelativePath(versionFilePath, options.cwd)));
-
-			if(util.type(options.callback) === 'function'){
-				options.callback(file.path, versionFilePath, file);
-			}
-			file.path = versionFilePath;
-			return callback(null, file);
-		});
-	};
-
+            return callback(null, file);
+        });
+    };
 	return stream;
 };
 
-exports.addVersion = addVersion;
-exports.getVersionMap = util.getVersionMap;
-exports.getSvnVersion = util.getSvnVersion;
-exports.getGitVersion = util.getGitVersion
-exports.cloneGitBaseRemote = util.cloneGitBaseRemote;
+Versions.prototype.setForTransform = function(cb){
+    return this.getForTransform( (version, file) => {
+        if (!version) return ;
+
+        var path = file.path;
+        var versionFilePath = this.formaterPath(path, version);
+
+        logger(chalk.yellow(relativePath(path, this.cwd)), '->', chalk.green(relativePath(versionFilePath, this.cwd)));
+        file.path = versionFilePath;
+
+        if(type(cb) === 'function'){
+            cb(path, versionFilePath);
+        }
+    });
+};
+
+// 返回缓存的文件版本记录
+Versions.prototype.getCache = function (path){
+	var cacheObj = cacheWare(this.cache);
+	if(path){
+		return cacheObj[path];
+	}else{
+		return Object.assign({}, cacheObj);
+	}
+}
+
+// 清除缓存的版本记录
+Versions.prototype.delCache = function (path){
+	var cacheObj = cacheWare(this.cache);
+	if(path){
+		if(cacheObj[path]) delete cacheObj[path];
+	}else{
+		for(var i in cacheObj){
+			delete cacheObj[i];
+		}
+	}
+}
+
+Versions.cloneGitBaseRemote = function(remoteUrl, branche, callback){
+	checkDotGit(function(err, isGitRepDir){
+    	if(err){
+			return callback(err);
+		}
+
+		if(isGitRepDir){
+			logger(chalk.blue('[Git] .git仓库目录已经存在'));
+			return callback(null);
+		}
+
+        var cmdStr = `git clone -s --bare -b ${branche||'master'} ${remoteUrl} ${dotGitPath}`;
+		logger(chalk.blue('[Git] clone远程分支到本地: ' + cmdStr));
+		exec(cmdStr, {}, function(err, out, stderr){
+			if (err) {
+				return callback(err);
+			}
+			logger(chalk.blue(out));
+			callback(null);
+		});
+	});
+};
+
+
+/****** util *****/
+
+function noop() {}
+
+// 获取相对cwd的相对路径
+function relativePath(filePath, cwd) {
+	return Path.relative(cwd || process.cwd(), filePath);
+}
+
+/**
+ * 获取变量类型
+ */
+function type (obj) {
+	if (obj == null) return obj + '';
+	return typeof obj === 'object' || typeof obj === 'function' ?
+		Object.prototype.toString.call(obj).slice(8, -1).toLowerCase() || 'object' :
+		typeof obj;
+}
+function isPlainObject(o) {
+  if (type(o) !== 'object') return false;
+
+  // If has modified constructor
+  var ctor = o.constructor;
+  if (type(ctor) !== 'function') return false;
+
+  // If has modified prototype
+  var prot = ctor.prototype;
+  if (type(prot) !== 'object') return false;
+
+  // If constructor does not have an Object-specific method
+  if (prot.hasOwnProperty('isPrototypeOf') === false) {
+    return false;
+  }
+
+  // Most likely a plain Object
+  return true;
+};
+
+
+function cacheWare(cache){
+	return isPlainObject(cache) ? cache : versionMap;
+}
+
+// 执行命令
+
+// 获取SVN 版本号
+function getSvnVersion (path, opt, callback) {
+	if(type(opt) === 'function'){
+		callback = opt;
+		opt = {};
+	}else if (!opt){
+		opt = {};
+	}
+
+	var cacheObj = cacheWare(opt.cache);
+	if( opt.cache !== false && type(cacheObj[path]) !== 'undefined' ){
+		return callback( cacheObj[path] );
+	}
+
+	var cmdArgs = [
+		'svn',
+		'info',
+		'"' + path + '"',
+	];
+	if (opt.usr) cmdArgs.push('--username ' + opt.usr);
+	if (opt.pwd) cmdArgs.push('--password ' + opt.pwd);
+
+	debug('[SVN]', cmdArgs.join(' '));
+	cmdArgs = cmdArgs.concat([
+		'--xml',
+		'--non-interactive',
+		'--no-auth-cache',
+		'--trust-server-cert',
+	]);
+
+	exec(cmdArgs.join(' '), function (err, stdout, stderr) {
+		var match = /<commit\s+revision="(\d+)">/i.exec(stdout) || [];
+		var version = match[1] || null;
+		if (!version) {
+			logger.error(chalk.red('[SVN] 获取版本失败：' + path));
+		}else if(opt.cache !== false){
+			cacheObj[path] = version;
+		}
+		callback(version);
+  });
+}
+
+function getGitVersion (path, opt, callback) {
+	if(type(opt) === 'function'){
+		callback = opt;
+		opt = {};
+	}else if (!opt){
+		opt = {};
+	}
+
+	var cacheObj = cacheWare(opt.cache);
+	if( opt.cache !== false && type(cacheObj[path]) !== 'undefined' ){
+		return callback( cacheObj[path] );
+	}
+
+	var cmdArgs = [
+		'git',
+		'--git-dir="' + dotGitPath +'"',
+		'log',
+		'-1',
+		'--pretty="%h"', // 使用7位短hash方式, 完整hash请使用%H
+		'"' + path + '"',
+	];
+
+	debug('[Git]', cmdArgs.join(' '));
+	exec(cmdArgs.join(' '), function (err, version, stderr) {
+		version = String(version).trim();
+		if (version.length !== 7) {
+			logger.error(chalk.red('[Git] 获取版本失败：' + path));
+			version = null;
+		}else if(opt.cache !== false){
+			cacheObj[path] = version;
+		}
+		callback(version);
+	});
+}
+
+/**
+ * 检查当前目录是否是git仓库目录
+ * returns 是git仓库则返回true
+ */
+function checkDotGit(callback){
+	exec('git log -1', {}, function(err, out){
+		if(err){
+			if(String(err).indexOf('fatal: Not a git repository') > -1){
+				return callback(null, false);
+			} else {
+				return callback('[Git] checkDotGit: ' + err);
+			}
+		}else{
+			return callback(null, true);
+		}
+	});
+}
+
+
+
+
+
+
+module.exports = Versions;
